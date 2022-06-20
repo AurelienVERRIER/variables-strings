b21751bc10d0e828bc8dfe848348320b57331066
AurelienVERRIER <aurelien.verrier@gmail.com>
2022-06-20 13:40:52 +0200

    index.js done

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log("Hello world!")
+const sentence = "My name is xxx"
+console.log(sentence)
+
+const name = "Aurel"
+console.log(name)
+
+const newSentence = `Nice to meet you ${name}`
+console.log(newSentence)
+
+const sentence2 = "I am a new text!"
+console.log(sentence2.length)
+
+const food = "croissant is meh"
+console.log(food.replace("meh", "so good"))
+
+const basic = "This is Cool"
+const basicUp = basic.toUpperCase()
+const basicDown = basic.toLocaleLowerCase()
+
+console.log(basic)
+console.log(basicUp)
+console.log(basicDown)
+
+const word = "banana"
+const letters = word.split("")
+console.log(letters)
+
+const age = "42"
+const template = `I'm ${age} years old`
+console.log(template)
